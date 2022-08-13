03ae859d1b8e22d24f42fbc424a60f5b721d6342
Tien Hai <tienhai488@gmail.com>
2022-08-13 22:10:03 +0700

    build base layout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Siderbar() {
-    return <h1>Siderbar</h1>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h1>Sidebar</h1>
+        </aside>
+    );
 }
 
 export default Siderbar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
 import Header from '~/components/Layout/components/Header';
 import Siderbar from './Sidebar';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Siderbar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
